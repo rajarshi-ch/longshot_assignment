884fcc7a3e7c0f1d996225cd01f1e5b83c266ca9
Rajarshi <rajarshibz@gmail.com>
2022-11-30 22:06:04 +0530

    added change plan button

--- views/mock_ui/components/userAvatar.tsx
+++ views/mock_ui/components/userAvatar.tsx
@@ -2,8 +2,8 @@
 
 export default function UserAvatar() {
   return (
-    <div className="inline-flex overflow-hidden relative justify-center items-center w-10 h-10 bg-indigo-600 rounded-full dark:bg-gray-600">
-      <span className="font-medium text-white dark:text-gray-300">KS</span>
+    <div className="inline-flex overflow-hidden relative justify-center items-center w-8 h-8 bg-indigo-600 rounded-full dark:bg-gray-600">
+      <span className="text-sm text-white dark:text-gray-300">KS</span>
     </div>
   );
 }
