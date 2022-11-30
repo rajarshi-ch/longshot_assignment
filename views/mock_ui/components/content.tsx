4f5272cd398d3d97b8836c2da1bfa1451d24ac2e
Rajarshi <rajarshibz@gmail.com>
2022-12-01 02:33:28 +0530

    Intent mapped to badges

--- views/mock_ui/components/content.tsx
+++ views/mock_ui/components/content.tsx
@@ -2,6 +2,7 @@ import HelpFab from "./helpFab";
 import Icon from "./icon";
 import IconButton from "./iconButton";
 import { AiOutlineRight } from "react-icons/ai";
+import { IntentBadge } from "./intentBadge";
 
 type IContentProps = {
   onSidebarHide: Function;
@@ -42,12 +43,52 @@ export default function Content({
                 <img src={`/usa.svg`} alt="" className="h-3" />
               </div>
               <div className="border-b-[1px] w-full mt-3 border-neutral-200 dark:border-neutral-700" />
+              <div className="flex flex-col xl:flex-row sm:flex-col max-w-[900px] w-full xl:space-x-2 xl:space-y-0  sm:space-y-2 sm:space-x-0 space-y-2 mx-auto mt-4">
+                <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg">
+                  Volume
+                  <div className="flex flex-row">
+                    <div className="text-xl font-bold">480</div>
+                    <img
+                      src={`/usa.svg`}
+                      alt=""
+                      className="h-3 align-middle self-center ml-2"
+                    />
+                  </div>
+                  <div className="border-b-[1px] w-full my-3 mx-3 border-neutral-200 dark:border-neutral-700" />
+                  Keyword Difficulty
+                  <div className="flex flex-row my-2">
+                    <div className="flex-column">
+                      <div className="text-xl font-bold">46 %</div>
+                      <div className="text-sm">Possible</div>
+                    </div>
+                    <img
+                      src={`/usa.svg`}
+                      alt=""
+                      className="h-3 align-middle self-center ml-2"
+                    />
+                  </div>
+                  Slightly more competition. You'll need well-structured and
+                  unique content appropriately optimized for your keywords.",
+                </div>
+                <div className="flex flex-col w-full h-100% max-h-[500px] items-stretch space-y-2">
+                  <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg grow">
+                    Intent
+                    <IntentBadge intent={0} />
+                  </div>
+                  <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg grow">
+                    abc
+                  </div>
+                  <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg grow">
+                    abc
+                  </div>
+                </div>
+              </div>
             </div>
-            <IconButton
+            {/* <IconButton
               icon="res-react-dash-sidebar-open"
               className="block sm:hidden"
               onClick={() => onSidebarHide()}
-            />
+            /> */}
           </div>
         </div>
       </div>
---
+++ views/mock_ui/components/intentBadge.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+
+const intentMap = {
+  0: {
+    type: "Commercial",
+    hover_text: "The user wants to investigate brands or services.",
+    color: { bg: "#FCE081", text: "#A75800", hover: "#ffca6e" },
+  },
+  1: {
+    type: "Informational",
+    hover_text: "The user wants to find an answer to a specific question.",
+    color: { bg: "#C4E5FE", text: "#006DCA", hover: "#61c6ff" },
+  },
+  2: {
+    type: "Navigational",
+    hover_text: "The user wants to find a specific page or site.",
+    color: { bg: "#EDD9FF", text: "#8649E1", hover: "#c59dfa" },
+  },
+  3: {
+    type: "Transactional",
+    hover_text: "The user wants to complete an action (conversion).",
+    color: { bg: "#9EF2C9", text: "#007C65", hover: "#11d6a6" },
+  },
+};
+
+export function IntentBadge({ intent }: { intent: number }) {
+  const { color: c, type } = intentMap[intent as keyof typeof intentMap];
+
+  return (
+    <div
+      className={clsx(
+        "text-xs font-semibold mr-2 px-2.5 py-0.5 rounded w-content",
+        `bg-[${c.bg}] text-[${c.text}] hover:bg-[${c.hover}]`
+      )}
+    >
+      {type}
+    </div>
+  );
+}
