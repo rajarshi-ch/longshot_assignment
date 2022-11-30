dada752ee338bec6e6de95a5f9a31fd47e8523a0
Rajarshi <rajarshibz@gmail.com>
2022-12-01 00:53:47 +0530

    Content layout

--- views/mock_ui/components/content.tsx
+++ views/mock_ui/components/content.tsx
@@ -18,32 +18,30 @@ export default function Content({
       <HelpFab toggleDarkMode={toggleDarkMode} />
       <div className="w-full h-screen hidden sm:block sm:w-20 xl:w-60 flex-shrink-0" />
       <div className=" h-screen flex-grow overflow-x-hidden overflow-auto flex flex-wrap content-start p-2">
-        <div className="w-full sm:flex p-2 items-end">
+        <div className="w-full sm:flex p-6 px-8 items-end">
           <div className="sm:flex-grow flex justify-between">
-            <div>
+            <div className="w-full">
               <div className="flex items-center">
                 <div>Keyword Explorer</div>
                 <AiOutlineRight className="mx-2" />
                 <div className="dark:text-white text-neutral-900 font-semibold">
                   Keyword Overview
                 </div>
-                {/* <div className="flex items-center p-2 bg-card ml-2 rounded-xl">
-                  <Icon
-                    path="res-react-dash-premium-star"
-                    className="w-8 h-8"
-                  />
-                  <div className="ml-2 font-bold text-premium-yellow">
-                    PREMIUM
-                  </div>
-                </div> */}
               </div>
-              <div className="flex items-center">
-                <Icon
-                  path="res-react-dash-date-indicator"
-                  className="w-3 h-3"
-                />
-                <div className="ml-2">October 26</div>
+              <div className="flex items-center mt-3">
+                <div className="text-xl dark:text-white text-neutral-900 font-bold">
+                  Keyword :
+                </div>
+                <div className="text-xl ml-2 font-bold">
+                  Shopping in barcelona
+                </div>
               </div>
+              <div className="flex items-center dark:text-white text-neutral-900 font-semibold space-x-1">
+                <div>Database: </div>
+                <div> United States </div>
+                <img src={`/usa.svg`} alt="" className="h-3" />
+              </div>
+              <div className="border-b-[1px] w-full mt-3 border-neutral-200 dark:border-neutral-700" />
             </div>
             <IconButton
               icon="res-react-dash-sidebar-open"
