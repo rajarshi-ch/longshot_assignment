1523b2cd77af1d085eda44ca7c1845e27f98b09a
Rajarshi <rajarshibz@gmail.com>
2022-12-01 10:14:50 +0530

    Aff to list button

--- views/mock_ui/components/content.tsx
+++ views/mock_ui/components/content.tsx
@@ -48,7 +48,15 @@ export default function Content({
               <div className="flex flex-col mx-auto mt-4 max-w-[900px] w-full ">
                 <DetailCards />
 
-                <GroupedButtons />
+                <div className="flex flex-row justify-between w-full">
+                  <GroupedButtons />
+                  <button
+                    type="button"
+                    className="text-white bg-gradient-to-r from-purple-600 to-indigo-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-md text-sm px-5 py-2.5 my-3 text-center"
+                  >
+                    Add to List
+                  </button>
+                </div>
 
                 <Table />
               </div>
