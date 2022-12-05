f00d11b78bba6e301e483243ee6a261bd809b793
Rajarshi <rajarshibz@gmail.com>
2022-12-06 03:38:47 +0530

    link fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
           </a>
 
           <a
-            href="/drag_n_drop"
+            href="/crud"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
